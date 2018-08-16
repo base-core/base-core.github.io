b8d96734bb5682ae1750d0eb0b983c7796c64a8a
Ruzerio <ruzero.io@gmail.com>
2018-08-16 17:26:17 +0300

    change active item on scroll

--- src/js/main.js
+++ src/js/main.js
@@ -16,3 +16,43 @@ $('.slider-nav').slick({
 	arrows: false,
 	focusOnSelect: true
 });
+
+$(function () {
+
+	function onScroll(event){
+	    var scrollPos = $(document).scrollTop() + 70;
+	    $('.menu a').each(function () {
+	        var currLink = $(this);
+	        var refElement = $(currLink.attr("href"));
+	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+	            $('.menu li').removeClass("active");
+	            currLink.parent().addClass("active");
+	        }
+	        else{
+	            currLink.parent().removeClass("active");
+	        }
+	    });
+	}
+	$(window).scroll(onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+})
