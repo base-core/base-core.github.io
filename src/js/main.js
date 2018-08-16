fc7aa93ac0187c98e4b2ef0b22f79cd666c8af02
Ruzerio <ruzero.io@gmail.com>
2018-08-16 17:00:36 +0300

    meida

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 //= anime.min.js
 
 //= banner-animation.js
+
+$('.slider-for').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	fade: true,
+	asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+	slidesToShow: 5,
+	slidesToScroll: 1,
+	asNavFor: '.slider-for',
+	arrows: false,
+	focusOnSelect: true
+});
