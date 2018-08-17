ffa7cdc46bb1d2597605dbf287886e96010ea9b9
Ruzerio <ruzero.io@gmail.com>
2018-08-17 15:29:37 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -49,7 +49,7 @@ $(function () {
         var menu = target;
         var $target = $(target);
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top;
+            'scrollTop': $target.offset().top
         }, 500, 'swing', function () {
             window.location.hash = target;
             $(document).on("scroll", onScroll);
