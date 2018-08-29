b10b1104f5bdb4e3c5afb3d81b3bad4d1f194651
Dmitriy Kovalev <d.g.kovalev@gmail.com>
2018-08-29 16:15:45 +0300

    medium api updating

--- src/js/main.js
+++ src/js/main.js
@@ -56,17 +56,18 @@ $(function () {
         });
     });
     
-
+// ---------- MEDIUM INTEGRATION API ---------- 
     $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40BaseCore', function(data) {
+        console.log(data);
         data.items.forEach( function(element, index) {
-            $('.medium__container').append(`<div class="medium__publication">
+            $('.medium__container').append(`<a target="_blank" href="${element.link}" class="medium__publication">
                 <div class="medium__publication__photo"><img width="318px" src="${element.thumbnail}"></div>
                 <div class="medium__publication__header">
                     <h4>${element.title}</h4></div>
                 <div class="medium__publication__article">
                     <p>${ element.description.split('p>')[1].split('.')[0].substr(0, 100)}</p>
                 </div>
-            </div>`)
+            </a>`)
         });
     });
 
