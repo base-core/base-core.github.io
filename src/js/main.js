ec978248c6fe8ec620be428dc8718ceb1de1640f
Ruzerio <ruzero.io@gmail.com>
2018-09-01 17:03:22 +0300

    team article

--- src/js/main.js
+++ src/js/main.js
@@ -58,21 +58,48 @@ $(function () {
     
 // ---------- MEDIUM INTEGRATION API ---------- 
     $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40BaseCore', function(data) {
-        var articles = data.items.slice(0, 3);
+
+        // remove team arcile & get three feeds items
+        var articles = data.items.filter(element => element.title !== "BaseCore Team: we create an investment tool of the new generation.").slice(0, 3);
         articles.forEach( function(element, index) {
             let description = element.description.split('p>')[1].split('</p')[0].substr(0, 100);
             description = description.substring(0, description.lastIndexOf(" "));
 
+            // template for display articles
             $('.medium__container').append(`<div class="medium__publication --medium-padding">
                 <div class="medium__publication__photo"><img width="318px" src="${element.thumbnail}"></div>
-                <h4><a href="${element.link}" class="medium__publication__header">${element.title}</a></h4>
+                <h4 class="publication__title"><a href="${element.link}" class="medium__publication__header">${element.title}</a></h4>
                 <div class="medium__publication__article">
-                    <p>${description}</p>
-                    <p><a class="details" target="_blank" href="${element.link}">Read more..</a></p>
+                    <p>${description}...</p>
+                </div>
+                <div class="link">
+                    <p><a class="details" target="_blank" href="${element.link}">Read more...</a></p>
                 </div>
             </div>`)
         });
+
+        // get team article 
+        var teamArticle = data.items.find(element => element.title == "BaseCore Team: we create an investment tool of the new generation.");
+        teamArticle.description = teamArticle.description.split('p>')[1].split('</p')[0].substr(0, 100);
+
+        // article template
+        $('.about_team').append(` <div><img class="about_team__photo" src="${teamArticle.thumbnail}"></div>
+            <div class="about_team__info">
+                <div class="about_team__header">
+                    <h2><a href="${teamArticle.link}">${teamArticle.title}</a></h2>
+                </div>
+                <div class="about_team__text">
+                    <p>${teamArticle.description}...</p>
+                    <p><a href="${teamArticle.link}">Read More...</a></p>
+                </div>
+            </div>`)
+
+
     });
+
+
+
+
 
 
     if (window.innerWidth <= 425) {
