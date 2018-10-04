6400432109ec1ce9437ca0f96ef0286953298b39
Ruzerio <ruzero.io@gmail.com>
2018-10-04 13:10:08 +0300

    video galery

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,81 @@ $('.slider-nav').slick({
     focusOnSelect: true
 });
 
+
+
+function onYouTubeIframeAPIReady() {
+    var videos = [ '7rfMe-2v68A', 'D68lxY-8wPA', 'VULKzVZCso0', 'L5eNAWbn6mQ', 'MFjcKxx-h7Y' ];
+    var playlistThumbs = $('.playlist-thumbs');
+    var prevBtn = $('#prev');
+    var nextBtn = $('#next');
+    var videoThumbs;
+    var currentIndex = 0;
+
+    videos.forEach(function(id, i){
+      var activeClass = '';
+      if ( i === currentIndex ) {
+        activeClass = 'is-active';
+      }
+      console.log(id)
+     $('.playlist-thumbs').append(`<li data-video-id="${id}" class="video-thumb ${activeClass}"><img src="https://img.youtube.com/vi/${id}/default.jpg"/></li>`)
+    });
+
+    videoThumbs = $('.video-thumb');
+
+    videoThumbs.click(function(){
+      currentIndex = videos.indexOf($(this).attr('data-video-id'));
+      player.loadVideoById(videos[currentIndex], 0, "large");
+      $(this).addClass('is-active').siblings().removeClass('is-active');
+      
+    })
+
+    initPlayer(videos[currentIndex]);
+
+
+    var player;
+    function initPlayer(id) {
+      console.log('initPlayer');
+      // This function creates an <iframe> (and YouTube player) after the API code downloads.
+      player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: id,
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+    }
+
+    prevBtn.on('click', playPrevVideo);
+    nextBtn.on('click', playNextVideo);
+
+    // The API will call this function when the video player is ready.
+    function onPlayerReady(event) {
+      console.log('onPlayerREady', event);
+      event.target.playVideo();
+    }
+
+    function onPlayerStateChange(event) {
+      if ( event.data === 0 ) { // video ended
+        playNextVideo();
+      }
+    }
+
+    function playNextVideo() {
+      currentIndex += 1;
+      player.loadVideoById(videos[currentIndex], 0, "large");
+      playlistThumbs.find(`[data-video-id="${videos[currentIndex]}"]`).addClass('is-active').siblings().removeClass('is-active');
+    }
+
+    function playPrevVideo() {
+        currentIndex -= 1;
+        player.loadVideoById(videos[currentIndex], 0, "large");
+        playlistThumbs.find(`[data-video-id="${videos[currentIndex]}"]`).addClass('is-active').siblings().removeClass('is-active');
+    }
+}
+
+
 $(function() {
 
     function onScroll(event) {
