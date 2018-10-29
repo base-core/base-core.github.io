f7c26a05f1ac60831a5c9883781841a0e846deeb
Ruzerio <ruzero.io@gmail.com>
2018-10-29 12:16:43 +0300

    fix request

--- src/js/main.js
+++ src/js/main.js
@@ -144,7 +144,6 @@ $(function() {
     });
 
     // ---------- MEDIUM INTEGRATION API ---------- 
-<<<<<<< HEAD
 
         $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fbscrinc', function(data) {
             // init arr with articles
@@ -187,61 +186,6 @@ $(function() {
                         </div>
                     </div>`)
             });
-
-=======
-    $.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2Fbscrinc', function(data) {
-        // init arr with articles
-        var articlesList = data.items;
-
-
-        console.log(articlesList)
-
-
-        // // sort by date
-        // articlesList.sort(function(a, b) {
-        //   a = new Date(a.pubDate);
-        //   b = new Date(b.pubDate);
-        //   return a > b ? -1 : a < b ? 1 : 0;
-        // });
-
-
-        // remove team arcile & get three feeds items
-        var articles = articlesList.filter(element => element.title !== "BaseCore Team: We create an investment tool of the new generation.").slice(0, 3);
-        articles.forEach(function(element, index) {
-            let description = element.description.split('p>')[1].split('</p')[0].substr(0, 100);
-            description = description.substring(0, description.lastIndexOf(" "));
-
-            // template for display articles
-            $('.medium__container').append(`<div class="medium__publication --medium-padding">
-                    <div class="medium__publication__photo"><a href="${element.link}" style="background-image: url(${element.thumbnail})"></a></div>
-                    <h4 class="publication__title"><a href="${element.link}" class="medium__publication__header">${element.title}</a></h4>
-                    <div class="medium__publication__article">
-                        <p>${description}...</p>
-                    </div>
-                    <div class="link">
-                        <p><a class="details" target="_blank" href="${element.link}">Read more...</a></p>
-                    </div>
-                </div>`)
-        });
-
-        // get team article 
-        var teamArticle = articlesList.find(element => element.title == "BaseCore Team: We create an investment tool of the new generation.");
-        teamArticle.description = teamArticle.description.split('p>')[1].split('</p')[0].substr(0, 100);
-        teamArticle.description = teamArticle.description.substring(0, teamArticle.description.lastIndexOf(" "));
-
-        // article template
-        $('.about_team').append(`<div class="about_team__photo"><a href="${teamArticle.link}" style="background-image: url(${teamArticle.thumbnail})"></a></div>
-                <div class="about_team__info">
-                    <div class="about_team__header">
-                        <h2 class="--align-text_left"><a href="${teamArticle.link}">${teamArticle.title}</a></h2>
-                    </div>
-                    <div class="about_team__text">
-                        <p>${teamArticle.description}...</p>
-                        <p><a href="${teamArticle.link}">Read More...</a></p>
-                    </div>
-                </div>`)
-    });
->>>>>>> 943833f883a57bf201fba224053b1ec594c3bcda
 
 
 
